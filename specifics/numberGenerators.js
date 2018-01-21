e8996e85982aaa92f42937a26d491d0349893215
Dmitry Yakimov <welldan97@gmail.com>
2018-01-21 22:12:17 +0000

    Make generators just regular functions

--- specifics/numberGenerators.js
+++ specifics/numberGenerators.js
@@ -1,23 +1,32 @@
-function* allDigitsGenerator(length = 9) {
-  yield Array.from({ length }, (_, i) => i + 1);
+function allNumbersByOneGenerator(length = Infinity) {
+  return index => {
+    if (index >= length || index < 0) throw 'Outside of range';
+    const done = index === length - 1;
+    return { value: Array.from({ length: index + 1 }, (_, i) => i + 1), done };
+  };
 }
 
-function* allDigitsByOneGenerator(length = Infinity) {
-  let digit = 1;
-  for (; digit < length; digit++) {
-    yield [digit];
-  }
-  return [digit];
+function allDigitsGenerator(length = 9) {
+  return index => {
+    if (index !== 0) throw 'Outside of range';
+    return { value: Array.from({ length }, (_, i) => i + 1), done: true };
+  };
 }
 
-function* oneDigitGenerator(digit) {
-  while (true) yield [digit];
+function oneDigitGenerator(digit) {
+  return index => {
+    if (index < 0) throw 'Outside of range';
+    return {
+      value: Array.from({ length: index + 1 }, (_, i) => digit),
+      done: false,
+    };
+  };
 }
 
 oneDigitGenerator.max = Infinity;
 
 module.exports = {
   allDigitsGenerator,
-  allDigitsByOneGenerator,
+  allNumbersByOneGenerator,
   oneDigitGenerator,
 };
--- specifics/numberGenerators.test.js
+++ specifics/numberGenerators.test.js
@@ -1,10 +1,44 @@
-const { allDigitsByOneGenerator } = require('./numberGenerators');
+const {
+  allNumbersByOneGenerator,
+  allDigitsGenerator,
+  oneDigitGenerator,
+} = require('./numberGenerators');
 
-describe('allDigitsByOneGenerator', () => {
-  it('gives digits one by one', () => {
-    const generator = allDigitsByOneGenerator(3);
-    expect(generator.next()).toEqual({ value: [1], done: false });
-    expect(generator.next()).toEqual({ value: [2], done: false });
-    expect(generator.next()).toEqual({ value: [3], done: true });
+describe('allNumbersByOneGenerator', () => {
+  it('gives ascending positive integers one by one', () => {
+    expect(allNumbersByOneGenerator(3)(0)).toEqual({
+      value: [1],
+      done: false,
+    });
+    expect(allNumbersByOneGenerator(3)(1)).toEqual({
+      value: [1, 2],
+      done: false,
+    });
+    expect(allNumbersByOneGenerator(3)(2)).toEqual({
+      value: [1, 2, 3],
+      done: true,
+    });
+  });
+});
+
+describe('allDigitsGenerator', () => {
+  it('gives all digits in ascending order once', () => {
+    expect(allDigitsGenerator()(0)).toEqual({
+      value: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      done: true,
+    });
+  });
+});
+
+describe('oneDigitGenerator', () => {
+  it('gives one digit again and again', () => {
+    expect(oneDigitGenerator(3)(0)).toEqual({
+      value: [3],
+      done: false,
+    });
+    expect(oneDigitGenerator(3)(3)).toEqual({
+      value: [3, 3, 3, 3],
+      done: false,
+    });
   });
 });
